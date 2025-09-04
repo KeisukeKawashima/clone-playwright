83da8dd877d5e933f8d0f99bd7df4127cfc663ac
KeisukeKawashima <codetrain.keisuke.kawashima@gmail.com>
2025-09-04 20:38:51 +0900

    wip

--- src/chromium.test.ts
+++ src/chromium.test.ts
@@ -1,8 +1,34 @@
-import { chromium } from './index';
+import { chromium , Browser} from "./index";
 
 describe('Chromium', () => {
+    let browser: Browser | null = null;
     test('should launch Chromium browser', async () => {
-        const result = await chromium.launch()
-        expect(result).toBeDefined()
+        browser = await chromium.launch()
+        expect(browser.close).toBeDefined()
+    });
+
+    test("should create new page", async () => {
+        browser = await chromium.launch();
+        const page = await browser.newPage();
+        expect(page).toBeDefined();
+    })
+
+    test('should get WebSocket URL from CHrome', async () => {
+        browser = await chromium.launch();
+        const wsUrl = await browser.getWebSocketUrl();
+        expect(wsUrl).toContain('ws://localhost:9222/devtools/page/')
+    })
+
+    test('should get real page title from Chrome', async () => {
+        browser = await chromium.launch();
+        const page = await browser.newPage();
+        
+        const title = await page.title();
+        expect(title).not.toBe("Mock Title");
+    })
+
+    afterEach( async () => {
+        browser?.close();
+        browser = null;
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
+import { spawn } from 'child_process';
+
+export interface Browser {
+    close: () => void;
+    newPage: () => Promise<Page>;
+    getWebSocketUrl: () => Promise<string>;
+}
+
+export interface Page {
+    goto: (url: string) => Promise<void>;
+    title: () => Promise<string>;
+    close: () => void;
+}
+
+
+
 export const chromium = {
-    async launch(): Promise<string> {
-        return "browser";
+    async launch(): Promise<Browser> {
+        const chromePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+        const chromeProcess = spawn(chromePath, [ "--headless", '--remote-debugging-port=9222']);
+        return {
+            close: () => chromeProcess.kill(),
+            newPage: async () => {
+                const page = new PageImpl();
+                return page;
+            },
+            getWebSocketUrl: async () => {
+                return 'ws://localhost:9222/devtools/page/';
+            }
+        }
+    },
+}
+
+class PageImpl implements Page {
+    async goto(url: string): Promise<void> {
+        // Implementation for navigating to a URL
+    }
+
+    async title(): Promise<string> {
+        // Implementation for getting the page title
+        return "Mock Title";
+    }
+
+    close(): void {
+        // Implementation for closing the page
     }
 }
